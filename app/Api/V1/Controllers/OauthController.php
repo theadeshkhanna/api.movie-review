9a03c234cd40c6d3145b8ec952ef8cc311eae4fb
Adesh Khanna <adeshkhanna271@gmail.com>
2020-05-26 21:44:53 +0530

    [setup] for google Oauth

---
+++ app/Api/V1/Controllers/OauthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Services\OauthService;
+use Laravel\Socialite\Facades\Socialite;
+
+class OauthController extends BaseController {
+    protected $oauthService;
+
+    public function __construct() {
+        $this->oauthService = new OauthService();
+    }
+
+    public function redirect() {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function getUser() {
+        $user = Socialite::driver('google')->user();
+    }
+}
